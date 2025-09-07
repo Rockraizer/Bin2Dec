4f6fa2a81ba4b1fa466d825f876d6e9c25c7ed09
dolan <dolan@MARTIN>
2025-09-07 18:08:24 +0200

    Add project files.

---
+++ Bin2Dec/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        new Program();
+    }
+
+    private string bin;
+    private double res;
+
+    public Program()
+    {
+        bin = Console.ReadLine();
+
+        if (bin == null)
+        {
+            Console.WriteLine("No binary number given!");
+            return;
+        }
+
+        if (bin.Length != 8)
+        {
+            Console.WriteLine("The given binary number is not 8 characters long!");
+            return;
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            int num = bin[i] - '0'; // konvertál char-ból int-re
+
+            if (num != 0 && num != 1)
+            {
+                Console.WriteLine("Only 0 and 1 can appear in the binary number!");
+                return;
+            }
+
+            res += Math.Pow(2, 7 - i) * num; // binárisból decimálissá alakítás
+        }
+
+        Console.WriteLine($"The binary number in decimal: {res}");
+    }
+}
